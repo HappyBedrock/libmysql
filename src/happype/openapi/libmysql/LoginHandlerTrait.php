7a59aeba9c67793cf2f0656e9dccf78a1ed1b8c1
VixikHD <gamak.pmmp@gmail.com>
2021-08-12 22:06:17 +0200

    Initialized Player Join Event

--- src/happype/openapi/libmysql/LoginHandlerTrait.php
+++ src/happype/openapi/libmysql/LoginHandlerTrait.php
@@ -4,15 +4,37 @@ declare(strict_types=1);
 
 namespace happype\openapi\libmysql;
 
+use happype\openapi\libmysql\event\InitializedPlayerJoinEvent;
 use happype\openapi\libmysql\event\PlayerLoginQueryReceiveEvent;
 use happype\openapi\libmysql\query\LazyRegisterQuery;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerLoginEvent;
+use function array_key_exists;
 
 trait LoginHandlerTrait {
+
+	/** @var bool[] */
+	protected array $initializedPlayers = [];
 
     public function onLogin(PlayerLoginEvent $event): void {
         QueryQueue::submitQuery(new LazyRegisterQuery($event->getPlayer()->getName(), DatabaseData::getInitTableList(), DatabaseData::getFetchTableList()), function (LazyRegisterQuery $query) use ($event): void {
             (new PlayerLoginQueryReceiveEvent($event->getPlayer(), $query))->call();
+            if($event->getPlayer()->spawned) {
+	            (new InitializedPlayerJoinEvent($event->getPlayer()))->call();
+	            return;
+            }
+
+	        $this->initializedPlayers[$event->getPlayer()->getName()] = true;
         });
+    }
+
+	/**
+	 * @priority HIGH
+	 */
+    public function onJoin(PlayerJoinEvent $event): void {
+    	if(array_key_exists($event->getPlayer()->getName(), $this->initializedPlayers)) {
+		    (new InitializedPlayerJoinEvent($event->getPlayer()))->call();
+		    unset($this->initializedPlayers[$event->getPlayer()->getName()]);
+	    }
     }
 }
---
+++ src/happype/openapi/libmysql/event/InitializedPlayerJoinEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace happype\openapi\libmysql\event;
+
+use pocketmine\event\Event;
+use pocketmine\player\Player;
+
+class InitializedPlayerJoinEvent extends Event {
+
+	public function __construct(
+		protected Player $player
+	) {}
+
+	public function getPlayer(): Player {
+		return $this->player;
+	}
+}
